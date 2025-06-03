6db9ac50edd1f74d0502bbe7580cc98eeed61caa
Spearkiller <bradley56789@gmail.com>
2025-06-03 18:05:06 +1200

    Fixed Saturation values being WAY off

--- src/main/java/forestry/core/items/ItemFruit.java
+++ src/main/java/forestry/core/items/ItemFruit.java
@@ -8,19 +8,19 @@ import java.util.Locale;
 public class ItemFruit extends ItemForestryFood {
 
 	public enum EnumFruit implements IItemSubtype {
-		CHERRY(1, 0.4f, 16),
-		WALNUT(1, 0.6f, 16),
-		CHESTNUT(1, 0.8f, 16),
+		CHERRY(1, 0.5f, 16),
+		WALNUT(1, 0.75f, 16),
+		CHESTNUT(1, 1.5f, 16),
 		LEMON,
 		PLUM,
-		DATES(1, 0.2f, 16),
-		PAPAYA(4, 3.2f, 32),
+		DATES(1, 0.75f, 16),
+		PAPAYA(4, 0.8f, 32),
 
 		PEAR,
 		ORANGE,
-		COCONUT(2, 4.2f, 64), //Not sure what the best stat is here. Low hunger, high saturation and use time seems fair???
-		OLIVE(1, 0.2f, 16),
-		FEIJOA(2, 1.2f, 16);
+		COCONUT(2, 1.6f, 64), //Not sure what the best stat is here. Low hunger, high saturation and use time seems fair???
+		OLIVE(1, 1f, 16),
+		FEIJOA(2, 0.6f, 16);
 
 		private final String name;
 
@@ -32,7 +32,7 @@ public class ItemFruit extends ItemForestryFood {
 			this.name = name().toLowerCase(Locale.ENGLISH);
 			//The default stats is the same as the vanilla apple.
 			this.heal = 4;
-			this.saturation = 2.4f;
+			this.saturation = 0.3f;
 			this.useTime = 32;
 		}
 
